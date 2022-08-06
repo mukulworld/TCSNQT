da309928de36dc56f551cee5f7417e2334001a1a
A. Mukul <60055486+mukulworld@users.noreply.github.com>
2022-08-06 13:03:29 +0530

    Create rotating array.cpp

---
+++ rotating array.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void rotate(int k,vector<int>array){
+
+  unordered_map<int,int>mapp;
+  
+  for(int i=0;i<array.size();i++)	mapp[array[i]] = i;
+  
+  for( auto x: mapp) array[(x.second+k)%array.size()] = x.first;
+  
+  for(auto x: array) cout<<x<<" ";
+}
+
+
+
+
+
+int main(){
+	int a,k;
+		cin>>a;
+	    cin>>k;
+	    vector<int>array;
+	    int x;
+	    
+	    for(int i=0; i<a; i++){
+	    	cin>>x;
+	    	array.push_back(x);
+		}
+	    
+	rotate(k,array);
+	
+	
+	return 0;
+}
